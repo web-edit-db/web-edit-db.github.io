80106460ae74cf5a424b4f340535f1fef4796159
hananf11 <hananf11@users.noreply.github.com>
2021-09-24 09:09:08 +0000

    Deploying to gh-pages from @ web-edit-db/web-edit-db@9b3d3c5452e389dd2e22a7f5535f6992cd067817 🚀

--- sw.js
+++ sw.js
@@ -113,17 +113,17 @@ define("./sw.js",['./workbox-600da2ae'], function (workbox) { 'use strict';
     "url": "assets/About.822e9462.js",
     "revision": "e36fac9d34bdc15d2620599e3da35979"
   }, {
-    "url": "assets/Columns.1beaa9d8.js",
-    "revision": "d17dd056ac4655922ecca6b302716eec"
-  }, {
     "url": "assets/Columns.88f63874.css",
     "revision": "d15a4ee8ea65a6e5b055e39a77ce022b"
   }, {
-    "url": "assets/Data.bf806b98.css",
-    "revision": "70446b74f8101d05e7c553ea45d37c98"
+    "url": "assets/Columns.e5fae571.js",
+    "revision": "0aa91c2031bf44fc3210ab2f9c7abf12"
   }, {
-    "url": "assets/Data.f1b87bd1.js",
-    "revision": "70dc25d30a8b16e4d5ed64fb372454d1"
+    "url": "assets/Data.bf44f62a.js",
+    "revision": "bcb45b6f5d52f2374cb192c3dec14f5f"
+  }, {
+    "url": "assets/Data.d666f7f1.css",
+    "revision": "205e46eebce7a7c1a1cfc212e8105b0f"
   }, {
     "url": "assets/directory-open.2b782ba5.js",
     "revision": "76626bd1c592233b112ec28163e6b54c"
@@ -152,11 +152,11 @@ define("./sw.js",['./workbox-600da2ae'], function (workbox) { 'use strict';
     "url": "assets/file-save.fb4972a3.js",
     "revision": "0edc4d96e6143d6ef1bb2f7348c34a1b"
   }, {
-    "url": "assets/index.c2f08d8b.css",
-    "revision": "b16dad16344bff7613495a64fb225566"
+    "url": "assets/index.dbd9b917.css",
+    "revision": "418e4b8fe270aadef0e65b18905a48a1"
   }, {
-    "url": "assets/index.e09cf3bb.js",
-    "revision": "d073b715a80b98bd7420284ca11198bd"
+    "url": "assets/index.fb1a27f8.js",
+    "revision": "5637f457b4f62957b377ebbe74c72acb"
   }, {
     "url": "assets/logo.9bfa92a2.png",
     "revision": "a142b0f39bf351a93098d585dfa69f86"
@@ -219,7 +219,7 @@ define("./sw.js",['./workbox-600da2ae'], function (workbox) { 'use strict';
     "revision": "420694e96d2049b190d1cd39bd326f51"
   }, {
     "url": "index.html",
-    "revision": "61fda94396edbbff884e4f3ce2b8732b"
+    "revision": "a05f9bdcbe36c5b3808e9cbbc18b2cb8"
   }, {
     "url": "./img/icons/android-chrome-192x192.png",
     "revision": "877f33163730442ab9a70eaad8819999"
